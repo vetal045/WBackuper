257886ea4715ed2a6822d2ebc17a7f202d2500df
Vladislav Krapivin <m.vladislav.krapivin@gmail.com>
2017-07-08 15:53:34 +0300

    Added pch.

---
+++ modules/BackupManager/src/pch.h
@@ -0,0 +1,24 @@
+#pragma once
+
+//
+// Standart C++ library includes.
+//
+
+#include <exception>
+
+//
+// Qt library includes.
+//
+
+#include <QBuffer>
+#include <QByteArray>
+#include <QUrl>
+#include <QFile>
+#include <QFileInfo>
+#include <QDir>
+#include <QList>
+#include <QMap>
+#include <QString>
+#include <QTextStream>
+#include <QDataStream>
+#include <QCryptographicHash>
---
+++ modules/ConsoleApp/src/pch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+//
+// Standart C++ library includes.
+//
+
+#include <exception>
+
+//
+// Qt library includes.
+//
+
+#include <QMap>
+#include <QString>
+#include <QTextStream>
